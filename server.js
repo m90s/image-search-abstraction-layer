3bdb7d643a485f62bb545b0e63892084d7f0b3b3
Mohammed alsokait <malsokait@gmail.com>
2016-11-14 00:39:25 +0000

    finished query call

--- server.js
+++ server.js
@@ -2,13 +2,46 @@ var express = require("express");
 var app = express();
 var url = require("url");
 var db = require("./db");
+var request = require("request");
+var api = require("./api_key");
+
+
+var API_URL = 'https://api.cognitive.microsoft.com/bing/v5.0/images/search';
+var API_IMG_COUNT = 10;
+
 
 app.get('/api/imagesearch/:query', function(req, res){
     var query = req.params.query;
     var offset = req.query.offset;
     
     db.insert(query, new Date().toUTCString());
-    res.end();
+    
+    var options = {
+      url: API_URL,
+      headers: {
+        'Ocp-Apim-Subscription-Key': api.API_KEY
+      },
+      qs: {
+        q: query,
+        offset: offset,
+        count: API_IMG_COUNT,
+        
+      }
+    };
+    
+    request(options, callback);
+    
+    function callback(error, response, body){
+      var results = [];
+      if(!error && response.statusCode == 200){
+        var imgs = JSON.parse(body);
+        for (var i in imgs.value) {
+          results.push({url: imgs.value[i].contentUrl, snippet: imgs.value[i].name, thumbnail: imgs.value[i].thumbnailUrl, context: imgs.value[i].hostPageDisplayUrl});
+        }
+        
+        res.json(results);
+      }
+    };
     
 });
 
@@ -25,4 +58,5 @@ app.get('/api/lastest/imagesearch/', function(req, res){
 
 app.listen(8080, function () {
   console.log('Running on 8080');
+  console.log(api.API_KEY);
 });
